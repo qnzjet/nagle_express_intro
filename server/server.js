faa0aba8a49702c6e9f94dda7bf57733818fb44f
HenryMarin-Prime <henrymarin@rocketmail.com>
2020-05-19 15:27:49 -0500

    use and listed added

--- server/server.js
+++ server/server.js
@@ -1,4 +1,20 @@
 //require express - give us a function
 const express = require('express');
 
-//create an instance of express
+//create an instance of express by calling the function above
+const app = express();
+const port = 5000;
+
+//serve up the static files from our public folder
+app.use(express.static('server/public'));
+
+
+//start the server
+app.listen(port, ()=>{
+    //functionality here
+    console.log('listening on port', port);
+})
+
+
+
+
